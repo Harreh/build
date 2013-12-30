bea85b1ae210f6c1d3e604ed733cd9cdeb21178e
Harry Dowe <harry@bournesit.co.uk>
2013-12-30 13:07:49 +0000

    Added pvp() and used title_for_layout

--- app/Controller/BuildOrdersController.php
+++ app/Controller/BuildOrdersController.php
@@ -15,11 +15,16 @@ class BuildOrdersController extends AppController {
 	}
 
 	public function pvt() {
+        $this->set('title_for_layout', 'PvT NaNiwa\'s Gateway expand');
 
 	}
 
 	public function pvz() {
-		
+        $this->set('title_for_layout', 'PvZ HerO’s Aggressive Stargate Build');
+	}
+
+	public function pvp() {
+        $this->set('title_for_layout', 'PvP');
 	}
 
 	public function db($id) {
