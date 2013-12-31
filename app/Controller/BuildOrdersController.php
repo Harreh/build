23f4331ad4e9118798d9fa658a4cece29a6d6c36
Harry Dowe <harry@bournesit.co.uk>
2013-12-31 13:43:37 +0000

    Added session component for setflash. Added add action and view action. Removed unused functions.

--- app/Controller/BuildOrdersController.php
+++ app/Controller/BuildOrdersController.php
@@ -2,16 +2,25 @@
 class BuildOrdersController extends AppController {
 
 	public $helpers = array('form', 'html', 'build');
+	public $components = array('Session');
 
 	public function index() {
 	}
 
-	public function view($id) {
-		
+	public function add() {
+		if ($this->request->is('post')) {
+			if ($this->BuildOrder->save($this->request->data)) {
+		    	$this->Session->setFlash(__('Your build has been saved'), 'flash_notification', array('class' => 'alert-success'));
+			    return $this->redirect(array('action' => 'index'));
+			}
+		    $this->Session->setFlash(__('Unable to save your build.'), 'flash_notification', array('class' => 'alert-warning'));
+		}
 	}
 
-	public function zerg() {
-
+	public function view($id) {
+		$this->BuildOrder->id = $id;
+		$this->set('title_for_layout',  $this->BuildOrder->field('title'));
+		$this->set('buildOrder', $this->BuildOrder->findById($id));
 	}
 
 	public function pvt() {
