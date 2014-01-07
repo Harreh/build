6a9895ff9727b5240a10ba265fb66ba936684120
Harry Dowe <harry@bournesit.co.uk>
2014-01-07 14:51:25 +0000

    Put html return strings on one line as it was adding extra whitespace to the HTML.

--- app/View/Helper/BuildHelper.php
+++ app/View/Helper/BuildHelper.php
@@ -20,24 +20,14 @@ class BuildHelper extends AppHelper {
 	}
 
 	public function event($title, $body) {
-		return "
-			<a class='list-group-item'>
-				<span class='badge'>&nbsp;".htmlspecialchars($title)."</span>
-				<span style='margin-left: 10px'>".htmlspecialchars($body)."</span>
-			</a>
-		";
+		return "<a class='list-group-item'><span class='badge'>&nbsp;".htmlspecialchars($title)."</span><span style='margin-left: 10px'>".htmlspecialchars($body)."</span></a>";
 	}
 
 	public function item($supply, $name) {
 		if (!is_int($supply) || !is_string($name)) {
 			throw new CakeException('Incorrect input types for BuildHelper/item');
 		}
-		return "
-			<a class='list-group-item'>
-				<span class='badge'><img src='/build/img/protoss_supply.gif'>&nbsp;$supply</span>
-				<span style='margin-left: 10px'>".htmlspecialchars($name)."</span>
-			</a>
-		";
+		return "<a class='list-group-item'><span class='badge'><img src='/build/img/protoss_supply.gif'>&nbsp;$supply</span><span style='margin-left: 10px'>".htmlspecialchars($name)."</span></a>";
 	}
 
 	public function note($note) {
@@ -58,23 +48,13 @@ class BuildHelper extends AppHelper {
 			throw new CakeException('Incorrect input values for BuildHelper/resource');
 		}
 
-		return "
-			<a class='list-group-item'>
-				<span class='badge'><img src='/build/img/$resource.gif'>&nbsp;$amount</span>
-				<span style='margin-left: 10px'>".htmlspecialchars($name)."</span>
-			</a>
-		";
+		return "<a class='list-group-item'><span class='badge'><img src='/build/img/$resource.gif'>&nbsp;$amount</span><span style='margin-left: 10px'>".htmlspecialchars($name)."</span></a>";
 	}
 
 	public function resources($minerals, $vespene, $body) {
 		if (!is_int($minerals) || !is_int($vespene) || !is_string($body)) {
 			throw new CakeException('Incorrect input type for BuildHelper/resources');
 		}
-		return "
-			<a class='list-group-item'>
-				<span class='badge'><img src='/build/img/minerals.gif'>&nbsp;$minerals&nbsp;<img src='/build/img/vespene-".$this->race.".gif'>&nbsp;$vespene</span>
-				<span style='margin-left: 10px'>".htmlspecialchars($body)."</span>
-			</a>
-		";
+		return "<a class='list-group-item'><span class='badge'><img src='/build/img/minerals.gif'>&nbsp;$minerals&nbsp;<img src='/build/img/vespene-".$this->race.".gif'>&nbsp;$vespene</span><span style='margin-left: 10px'>".htmlspecialchars($body)."</span></a>";
 	}
 }
