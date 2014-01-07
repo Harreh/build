1e8434c6270c8ae664281a557e3b08d089757f07
Harry Dowe <harry@bournesit.co.uk>
2014-01-07 14:48:35 +0000

    Updated add function to redirect to the newly added build. Added view and edit function.

--- app/Controller/BuildOrdersController.php
+++ app/Controller/BuildOrdersController.php
@@ -12,15 +12,44 @@ class BuildOrdersController extends AppController {
 		if ($this->request->is('post')) {
 			if ($this->BuildOrder->save($this->request->data)) {
 		    	$this->Session->setFlash(__('Your build has been saved'), 'flash_notification', array('class' => 'alert-success'));
-			    return $this->redirect(array('action' => 'index'));
+			    return $this->redirect(array('action' => 'view', $this->BuildOrder->id));
 			}
 		    $this->Session->setFlash(__('Unable to save your build.'), 'flash_notification', array('class' => 'alert-warning'));
 		}
 	}
 
 	public function view($id) {
+		if (!$id) {
+		    throw new NotFoundException(__('Invalid build.'));
+		}
+
+		$buildOrder = $this->BuildOrder->findById($id);
+		if (!$buildOrder) {
+		    throw new NotFoundException(__('Invalid build.'));
+		}
 		$this->BuildOrder->id = $id;
-		$this->set('title_for_layout',  $this->BuildOrder->field('title'));
+		$this->set('title_for_layout',  $buildOrder['BuildOrder']['title']);
+		$this->set('buildOrder', $buildOrder);
+	}
+
+	public function edit($id = null) {
+		if (!$id) {
+		    throw new NotFoundException(__('Invalid build.'));
+		}
+
+		$buildOrder = $this->BuildOrder->findById($id);
+		if (!$buildOrder) {
+		    throw new NotFoundException(__('Invalid build.'));
+		}
+
+		$this->BuildOrder->id = $id;
+		if ($this->request->is('post')) {
+			if ($this->BuildOrder->save($this->request->data)) {
+				$this->Session->setFlash(__('Your changes have been saved.'), 'flash_notification', array('class' => 'alert-success'));
+				return $this->redirect(array('action' => 'edit', $id));
+			}
+			$this->Session->setFlash(__('Unable to save your changes.'), 'flash_notification', array('class' => 'alert-warning'));
+		}
 		$this->set('buildOrder', $this->BuildOrder->findById($id));
 	}
 }
