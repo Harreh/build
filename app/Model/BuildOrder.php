b69f4178eabdee173209fd4fc50480d41e55411a
Harry Dowe <harry@bournesit.co.uk>
2014-01-09 15:16:30 +0000

    Added $belongsTo and isOwnedBy() to BuildOrder (model).

--- app/Model/BuildOrder.php
+++ app/Model/BuildOrder.php
@@ -1,5 +1,15 @@
 <?php
 
 class BuildOrder extends AppModel {
+	
+	public $belongsTo = array(
+	    'User' => array(
+	        'className' => 'User',
+	        'foreignKey' => 'user_id'
+	    )
+	);
 
+	public function isOwnedBy($buildOrder, $user) {
+	    return $this->field('id', array('id' => $buildOrder, 'user_id' => $user)) === $buildOrder;
+	}
 }
